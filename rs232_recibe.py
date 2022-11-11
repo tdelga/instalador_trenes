037edda7b394cb94ffe2d4c4184fd77692c51460
tdelga <toto.bocaa@gmail.com>
2022-11-10 23:21:23 -0300

    mejore comment

--- rs232_recibe.py
+++ rs232_recibe.py
@@ -1,4 +1,5 @@
 # Este codigo, escucha el puerto serial, en este caso /dev/pts/2, y en caso de que se envie cualquier dato, apaga el sistema
+# instalar la libreria serial con el comando 'pip install pyserial' 
 
 import serial
 import os
