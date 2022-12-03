dafec602734bfe715f8d9dce475a74cc4ba9b31e
frannale <franaledelrojo@gmail.com>
2022-12-03 11:14:39 -0300

    asd

--- start_lector.sh
+++ start_lector.sh
@@ -2,6 +2,7 @@
 cd  /home/user/LECTOR_RFID/
 git pull https://github.com/tdelga/instalador_trenes.git
 sleep 20
+echo Servicio levantado
 python3 rs232_recibe.py &
 java -jar /home/user/LECTOR_RFID/Lector_v1.jar
 echo Servicio levantado
