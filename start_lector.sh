3078cac95805a9ad394819be4d24c506238498ab
tdelga <48927952+tdelga@users.noreply.github.com>
2022-12-05 09:13:10 -0300

    arreglado el start lector

--- start_lector.sh
+++ start_lector.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 cd  /home/user/LECTOR_RFID/
-git pull https://github.com/tdelga/instalador_trenes.git
-sleep 20
+
+git remote update 
+
+sleep 5
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    git pull https://github.com/tdelga/instalador_trenes.git
+else
+    echo "Diverged"
+fi
+
+sleep 10
 echo Servicio levantado
 python3 rs232_recibe.py &
 java -jar /home/user/LECTOR_RFID/Lector_v1.jar
