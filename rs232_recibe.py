bc62a0748dad87ac5f30866f7037150938bd4094
tdelga <48927952+tdelga@users.noreply.github.com>
2023-01-05 20:22:34 -0600

    Update rs232_recibe.py

--- rs232_recibe.py
+++ rs232_recibe.py
@@ -6,10 +6,21 @@ import os
 import time
 from serial import SerialException
 
+time_to_check = 10
+start_time = time.time()
 
 while True:
     try:
         serBarCode = serial.Serial('/dev/pts/2', 9600, timeout=1)
+        # Get the current time
+        current_time = time.time()
+        time_passed = current_time - start_time
+
+        # Check if the desired amount of time has passed
+        if time_passed > time_to_check:
+            print("REINICIANDO")
+        break
+        print("EJECUTANDO")
         #read data from serial port
         if(serBarCode.read(4) != b""):
             serBarCode.close()
@@ -17,4 +28,3 @@ while True:
     except SerialException:
         print("Error al escuchar el puerto serial")
         time.sleep(1)
-
